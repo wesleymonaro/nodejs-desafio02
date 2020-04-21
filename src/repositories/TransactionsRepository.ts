4ca2886844f37763f698887fbc47fca05011da52
Wesley Monaro <wesleymonaro@hotmail.com>
2020-04-20 22:25:04 -0300

    finish app

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,32 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { transactions } = this;
+    let income = 0;
+    let outcome = 0;
+    let total = 0;
+    transactions.forEach(t => {
+      if (t.type === 'income') {
+        income += t.value;
+        total += t.value;
+      } else if (t.type === 'outcome') {
+        outcome += t.value;
+        total -= t.value;
+      }
+    });
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
